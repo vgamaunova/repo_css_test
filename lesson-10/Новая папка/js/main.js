132aab4a551d9993b82dc79a09c6a0bfbd853cf3
vgamaunova <viktoriagamaunova@gmail.com>
2015-09-30 20:27:56 +0300

    [+] add lesson-11 and new lesson-10

---
+++ lesson-10/Новая папка/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	$("#owl-demo").owlCarousel({
+  		items:1,
+    });
+	$(".owl-carousel-list").owlCarousel({
+  		items:4,
+  		 navigationText:[" ", " "],
+    navigation: true,
+    });
+});
