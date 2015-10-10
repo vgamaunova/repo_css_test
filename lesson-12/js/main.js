e2ad509171a59ae0937b61f6bf693ff0a8c9e5f6
vgamaunova <viktoriagamaunova@gmail.com>
2015-10-10 17:29:15 +0300

    [*] modified

--- lesson-12/js/main.js
+++ lesson-12/js/main.js
@@ -9,4 +9,25 @@ $(document).ready(function(){
     $('.main-button__img').on('click',function(){
     	$('.nav-main').toggleClass('open');
     })
+	$('.form-thumb__title').on('click',function(e){
+		e.preventDefault();
+
+		var $this = $(this),
+			item = $this.closest('.form-thumb'),
+			list = $this.closest('.form-box'),
+			items = list.find('.form-thumb'),
+			content = item.find('.form-thumb__wrapper'),
+			otherContent = list.find('.form-thumb__wrapper'),
+			duration = 300;
+		if (!item.hasClass('active')) {
+			items.removeClass('active');
+			item.addClass('active');
+			otherContent.stop(true, true).slideUp(duration);
+			content.slideDown(duration);
+		}else{
+			content.slideUp(duration);
+			item.removeClass('active');
+		}
+
+	});
 });
